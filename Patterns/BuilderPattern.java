c422681fc1396494783903f30a769085bb3f6aca
Akki <apple@Akki-Macbook.local>
2026-05-29 10:55:09 +0530

    coding patterns

---
+++ Patterns/BuilderPattern.java
@@ -0,0 +1,70 @@
+package Patterns;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BuilderPattern {
+    public static void main(String[] args) {
+
+        HttpRequest request = new HttpRequest.Builder().requestUri("https://google.com").method("POST").build();
+        request.send();
+    }
+}
+
+
+class HttpRequest {
+    private String baseUrl;
+    private String requestUri;
+    private int port;
+    private String method;
+    private Map<String, String> header;
+
+
+    public void send(){
+        System.out.println("HTTP Request sent for URL: " + this.method + " "+  this.requestUri);
+    }
+
+    public static class Builder {
+        private final HttpRequest request = new HttpRequest();
+
+        public Builder baseUrl(String baseUrl){
+            request.baseUrl=baseUrl;
+            return this;
+        }
+
+        public Builder requestUri(String requestUri){
+            request.requestUri=requestUri;
+            return this;
+        }
+
+        public Builder port(int port){
+            request.port=port;
+            return this;
+        }
+
+        public Builder method(String method){
+            request.method=method;
+            return this;
+        }
+
+        public Builder headers(String key, String value){
+            if (request.header == null) {
+                request.header = new HashMap<>();
+            }
+            request.header.put(key, value);
+            return this;
+        }
+
+        public HttpRequest build(){
+            if(request.requestUri == null){
+                throw new IllegalStateException("URL cannot be null");
+            }
+            if(request.method == null){
+                throw new IllegalStateException("Method cannot be null");
+            }
+
+            return request;
+        }
+
+    }
+}
---
+++ Patterns/FactoryPatternsDemo.java
@@ -0,0 +1,77 @@
+package Patterns;
+
+import java.util.Objects;
+
+class FactoryPatternsDemo {
+    public static void main(String[] args) {
+
+            Notification emailService = NotificationFactory.create(NotificationType.EMAIL);
+            emailService.sendNotification("Hi User, try our new Product");
+
+
+            Notification smsService = NotificationFactory.create(NotificationType.SMS);
+            smsService.sendNotification("Hi, User, your transaction was successful");
+
+            Notification whatsappService = NotificationFactory.create(NotificationType.WHATSAPP);
+            whatsappService.sendNotification("This item from your wishlist is on SALE!, Checkout NOW");
+
+            Notification test = NotificationFactory.create(null);
+            test.sendNotification("YO");
+    }
+}
+
+
+
+/**
+ * Factory Design Pattern is a Creational design pattern that provides an interface for creating objects in a superclass,
+ * but allows subclasses to alter the type of objects that will be created. 
+ * It promotes loose coupling by eliminating the need to bind application-specific classes into the code. 
+ * The client code interacts with the factory method, which creates and returns the appropriate object based on the input parameters or configuration.
+ */
+
+
+interface Notification{
+    void sendNotification(String message);
+}
+
+enum NotificationType { EMAIL, SMS, WHATSAPP };
+
+
+class EmailNotification implements Notification {
+    public void sendNotification(String message) {
+        System.out.println("Sending Email Notification: " + message);
+    }
+}
+
+class SMSNotification implements Notification {
+    public void sendNotification(String message){
+        System.out.println("Sending SMS Notification: " + message);
+    }
+}
+
+class WhatsappNotification implements Notification {
+    public void sendNotification(String message){
+        System.out.println("Sending Whatsapp Notification: " + message);
+    }
+}
+
+
+
+
+class NotificationFactory {
+    public static Notification create(NotificationType type){
+
+
+        if(type == null){
+            throw new IllegalArgumentException("Notification type cannot be null");
+        }
+
+        return switch (type) {
+            case NotificationType.EMAIL -> new EmailNotification();
+            case NotificationType.SMS -> new SMSNotification();
+            case NotificationType.WHATSAPP -> new WhatsappNotification();
+        };
+
+    }
+
+}
